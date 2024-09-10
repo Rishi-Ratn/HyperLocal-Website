4b9de28b6e86080315931c0be96a319e40e4d316
Rishi_Ratn <imrishiratn009@gmail.com>
2024-09-11 01:15:15 +0530

    fix5

--- backend/client/src/components/StarterPage.js
+++ backend/client/src/components/StarterPage.js
@@ -9,7 +9,7 @@ const StarterPage = () => {
   useEffect(() => {
     const fetchSocieties = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/society');
+        const response = await axios.get('/api/society');
         setSocieties(response.data);
         
       } catch (error) {
